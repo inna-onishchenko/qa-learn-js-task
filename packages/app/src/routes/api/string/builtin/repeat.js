60dcd51788471640b89d244e201ed526a7fa8d98
Inna Onishchenko <inna.onishchenko@betterme.world>
2026-05-15 00:04:12 +0300

    Prettied format

--- packages/app/src/routes/api/string/builtin/repeat.js
+++ packages/app/src/routes/api/string/builtin/repeat.js
@@ -3,22 +3,22 @@ const { Router } = require("express");
 const router = Router();
 
 router.post("/repeat", (req, res) => {
-    const strRepeat = req.body.str;
-    const count = req.body.count;
-    let resultRepeat = "";
-    let resultMessage = "";
-        if (typeof strRepeat !== "string") {
-        resultMessage = "Text to repeat should be a string";
-        res.statusCode = 400;
-        res.json({ status: "bad_request", language: "javascript", message: resultMessage });
-    } else if (count < 0) {
-        resultMessage = "Count should be a non-negative integer";
-        res.statusCode = 400;
-        res.json({ status: "bad_request", language: "javascript", message: resultMessage });
-    } else {
-        resultRepeat = strRepeat.repeat(count);
-        res.json({ status: "ok", language: "javascript", message: resultRepeat });
-    }
+  const strRepeat = req.body.str;
+  const count = req.body.count;
+  let resultRepeat = "";
+  let resultMessage = "";
+  if (typeof strRepeat !== "string") {
+    resultMessage = "Text to repeat should be a string";
+    res.statusCode = 400;
+    res.json({ status: "bad_request", language: "javascript", message: resultMessage });
+  } else if (count < 0) {
+    resultMessage = "Count should be a non-negative integer";
+    res.statusCode = 400;
+    res.json({ status: "bad_request", language: "javascript", message: resultMessage });
+  } else {
+    resultRepeat = strRepeat.repeat(count);
+    res.json({ status: "ok", language: "javascript", message: resultRepeat });
+  }
 });
 
 module.exports = router;
